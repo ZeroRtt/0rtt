4a9cec60537983367489727003457ce6ca4fd8d9
yayanyang <yayanyang@gmail.com>
2025-08-16 21:08:14 +0800

    remove all

--- crates/quico/src/lib.rs
+++ crates/quico/src/lib.rs
@@ -1,28 +1,12 @@
 //! Poll for readiness events on masive quiche connecitons.
 #![cfg_attr(docsrs, feature(doc_cfg))]
 
+#[allow(unused)]
 mod utils;
 
 mod errors;
 pub use errors::*;
 
-mod conn;
-mod readiness;
-
-mod events;
-pub use events::*;
-
-mod group;
-pub use group::*;
-
 #[cfg(feature = "server")]
 #[cfg_attr(docsrs, doc(cfg(feature = "server")))]
-mod address_validation;
-#[cfg(feature = "server")]
-pub use address_validation::*;
-
-#[cfg(feature = "server")]
-#[cfg_attr(docsrs, doc(cfg(feature = "server")))]
-mod acceptor;
-#[cfg(feature = "server")]
-pub use acceptor::*;
+pub mod validation;
---
+++ crates/quico/tests/quic.rs
@@ -0,0 +1,49 @@
+use quiche::Config;
+
+#[allow(unused)]
+fn mock_config(is_server: bool) -> Config {
+    use std::path::Path;
+
+    let mut config = Config::new(quiche::PROTOCOL_VERSION).unwrap();
+
+    config.set_initial_max_data(10_000_000);
+    config.set_initial_max_stream_data_bidi_local(1024 * 1024);
+    config.set_initial_max_stream_data_bidi_remote(1024 * 1024);
+    config.set_initial_max_streams_bidi(3);
+    config.set_initial_max_streams_uni(100);
+
+    config.verify_peer(true);
+
+    // if is_server {
+    let root_path = Path::new(env!("CARGO_MANIFEST_DIR"));
+
+    log::debug!("test run dir {:?}", root_path);
+
+    if is_server {
+        config
+            .load_cert_chain_from_pem_file(root_path.join("cert/server.crt").to_str().unwrap())
+            .unwrap();
+
+        config
+            .load_priv_key_from_pem_file(root_path.join("cert/server.key").to_str().unwrap())
+            .unwrap();
+    } else {
+        config
+            .load_cert_chain_from_pem_file(root_path.join("cert/client.crt").to_str().unwrap())
+            .unwrap();
+
+        config
+            .load_priv_key_from_pem_file(root_path.join("cert/client.key").to_str().unwrap())
+            .unwrap();
+    }
+
+    config
+        .load_verify_locations_from_file(root_path.join("cert/rasi_ca.pem").to_str().unwrap())
+        .unwrap();
+
+    config.set_application_protos(&[b"test"]).unwrap();
+
+    config.set_max_idle_timeout(60000);
+
+    config
+}
